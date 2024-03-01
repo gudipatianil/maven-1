d026c6c0c02f3ed92c17f4ebb1e17834f431b186
gudipatianil <152320354+gudipatianil@users.noreply.github.com>
2024-03-01 22:27:49 +0530

    Update MyPortlet.java

--- src/main/java/InfoTech/MyPortlet.java
+++ src/main/java/InfoTech/MyPortlet.java
@@ -64,7 +64,7 @@ public class MyPortlet extends GenericPortlet {
     }
 
     public void destroy() {
-        normalView = null;
+        normalView  jj = null;
         maximizedView = null;
         helpView = null;
         super.destroy();
